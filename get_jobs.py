4f892530d2e9b312998b6f6331cbad7a04329e98
tizet <tienn.hcmus@gmail.com>
2018-09-24 16:41:40 +0700

    add file

---
+++ get_jobs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sqlite3
+from bs4 import BeautifulSoup
+import requests
+
+base_url = 'https://github.com'
+jobs_url = 'https://github.com/awesome-jobs/vietnam/issues?page='
+
+
+def get_jobs():
+    result = []
+    for page in range(1,12):
+        html_data = requests.get(jobs_url + str(page)).text
+        soup = BeautifulSoup(html_data, 'html.parser')
+        jobs_page = soup.find_all('div', attrs={'class': 'float-left col-9 lh-condensed p-2'})
+        for job in jobs_page:
+            # add job info to list result
+            # [job name, job url, create time
+            result.append((job.find('a').string.strip(),
+                           base_url + job.find('a')['href'],
+                           job.find('relative-time').string))
+    return result
+
+
+def create_jobs_db(jobs):
+    conn = sqlite3.connect('jobs.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE jobs
+                 (job_name, job_url, job_time)''')
+    for job in jobs:
+        c.execute("INSERT INTO jobs VALUES(?, ?, ?)", job)
+    conn.commit()
+    conn.close()
+
+
+def main():
+    jobs = get_jobs()
+    # for job in jobs:
+    #     print(job)
+    create_jobs_db(jobs)
+    print('Done, open file jobs.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ jobs_web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_jobs():
+    jobs = sqlite3.connect('jobs.db').cursor().execute('SELECT * FROM jobs')
+    return render_template('index.html', jobs = jobs)
+
+
+if __name__ == '__main__':
+    # for job in jobs:
+    #     print(job)
+    app.run(debug=True)
